16c3be8a08c4393a27505db6c2571bd7568b3c1d
wangjianqiang1992 <1044230353@qq.com>
2019-07-30 23:57:19 +0800

    初始化项目

---
+++ DYZB/DYZB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DYZB
+//
+//  Created by qiangzi on 2019/7/30.
+//  Copyright © 2019年 WJQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
